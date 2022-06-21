bc4266f94b2e54977e70c588eda69d052329165f
Bibi4kovIgor <work.bibi4kov@gmail.com>
2022-06-21 21:53:28 +0300

    Added additional changes into output text Hello 3

--- src/com/bibichkov/Main.java
+++ src/com/bibichkov/Main.java
@@ -3,6 +3,6 @@ package com.bibichkov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World1!");
+        System.out.println("Hello World3!");
     }
 }
